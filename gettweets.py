429d2bbebfc66581a19e4256c5d5baefb09c9b73
Aditya-Rohan-Singh <98125056+Aditya-Rohan-Singh@users.noreply.github.com>
2022-06-11 14:08:21 -0700

    Added files

---
+++ gettweets.py
@@ -0,0 +1,25 @@
+import tweepy as tw
+import csv
+
+consumer_key= '5limczXPkrQXwSKZBgr22xvRY'
+consumer_secret= 'dnxoBGO2F0DptXDu6rQfBFf6E9tqWDFSh5SYvbh1mNAjKb8QhG'
+access_token= '110668153-9xkWmU2L83wl6EyrYAocda9ITYsXeb7BThzbbDiv'
+access_token_secret= '2NgrIpmwUjJU33FDhNtSs0WZNmnxs3Hjv4fbCfmZfNQZR'
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+search_words = "Depp"
+
+#date_since = "2021-10-01"
+
+tweets = tw.Cursor(api.search_tweets, q=search_words, lang="en").items(2000)
+
+df = open('Tweets.csv', 'a')
+
+write=csv.writer(df)
+
+for tweet in tweets:
+    write.writerow({tweet.text.encode('utf-8')})  
+df.close()
